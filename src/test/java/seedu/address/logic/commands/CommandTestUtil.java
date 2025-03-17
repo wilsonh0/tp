7a78b1c6b50fa1c84061d937456b7692cbfa71b6
Wilson Ho <wilsonhjda@gmail.com>
2025-03-17 20:28:34 +0800

    Update test data to reflect new parameters so test cases work again

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -28,12 +28,16 @@ public class CommandTestUtil {
 
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
+    public static final String VALID_NRIC_AMY = "T0000001B";
+    public static final String VALID_NRIC_BOB = "T0000002B";
     public static final String VALID_PHONE_AMY = "11111111";
     public static final String VALID_PHONE_BOB = "22222222";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_HIRE_AMY = "2021-01-01";
+    public static final String VALID_HIRE_BOB = "2022-02-02";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
