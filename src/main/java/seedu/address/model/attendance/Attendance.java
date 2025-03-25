50213bd8a3d299b46eb8704dc1901e0edc13c006
senshir <shishirsen2004@gmail.com>
2025-03-26 03:34:24 +0800

    Fix Attendance style errors

--- src/main/java/seedu/address/model/attendance/Attendance.java
+++ src/main/java/seedu/address/model/attendance/Attendance.java
@@ -1,7 +1,5 @@
 package seedu.address.model.attendance;
 
-import seedu.address.model.leave.Leave;
-
 /**
  * Represents the attendance details for a person in the address book.
  * Stores information such as the total number of working days, the number of absent days,
@@ -11,10 +9,12 @@ import seedu.address.model.leave.Leave;
  * The attendance rate is initially set to 100% and is recalculated whenever the absent day count is updated.
  */
 public class Attendance {
+    public static final String MESSAGE_CONSTRAINTS = "Absentees should be indicated by their NRIC "
+        + "and each absentee's NRIC should be separated by at least one whitespace.";
+
     // Stores the number of working days so far in that year
     private static int workDayCount = 0;
-    public static final String MESSAGE_CONSTRAINTS = "Absentees should be indicated by their NRIC "
-            + "and each absentee's NRIC should be separated by at least one whitespace.";
+
     private int absentDayCount;
     // Stores the current attendance rate for this person in that year, initialized as 100% first
     private double attendanceRate;
