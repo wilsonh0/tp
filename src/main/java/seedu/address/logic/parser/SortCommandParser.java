2ad5021216eeef91a48ce86db0f799e9dd3ee40b
aaronlim02 <e1123168@u.nus.edu>
2025-03-26 20:22:40 +0800

    Implement sorting command logic

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import seedu.address.logic.commands.AddTagCommand;
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -12,8 +13,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class SortCommandParser implements Parser<SortCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the ViewCommand
-     * and returns a ViewCommand object for execution.
+     *
+     *
      * @throws ParseException if the user input does not conform to the expected format
      */
     public SortCommand parse(String args) throws ParseException {
@@ -21,15 +22,13 @@ public class SortCommandParser implements Parser<SortCommand> {
         String trimmedArgs = args.trim();
 
         if (trimmedArgs.isEmpty()) {
-            throw new ParseException(SortCommand.MESSAGE_USAGE);
-        }
-
-        try {
-            String attribute = trimmedArgs.split(" ")[0]; //ParserUtil.parseIndex(trimmedArgs);
-            String direction = trimmedArgs.split(" ")[1];
-            return new SortCommand(attribute, direction);
-        } catch (NumberFormatException e) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
+
+        String[] parameters = trimmedArgs.split(" ", 2);
+        if (parameters.length < 2 || parameters[1].trim().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+        }
+        return new SortCommand(parameters[0], parameters[1]);
     }
 }
