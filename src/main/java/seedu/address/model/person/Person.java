2c4c0a9c7796b7e14b8896ffe9a0e89b9ab873c9
aaronlim02 <e1123168@u.nus.edu>
2025-04-02 15:23:41 +0800

    Fix Person.java and TypicalPersons.java

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -155,7 +155,7 @@ public class Person {
         }
 
         return otherPerson != null
-            && otherPerson.getName().equals(getName());
+            && otherPerson.getNric().equals(getNric());
     }
 
     /**
